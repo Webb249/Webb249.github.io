fe6f0dec044e96d30b9fc5d6a44a9884c2e1b439
Webb249 <tom.webb249@gmail.com>
2021-01-19 22:03:59 +0000

    Add javascript file with load nav bar code

---
+++ essentials.js
@@ -0,0 +1,7 @@
+function load_navigation() {
+	document.write("<div class=\"topnav\">"
+		+ "<a href=\"index.html\">Home</a>"
+		+ "<a href=\"about.html\">About Me</a>"
+		+ "<a href=\"contact.html\">Contact</a>"
+		+ "</div>;")
+}
