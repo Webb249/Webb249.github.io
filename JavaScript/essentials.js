2e7a804b338bbee3661194966557459dd6c88dfb
Webb249 <tom.webb249@gmail.com>
2021-01-20 18:31:53 +0000

    Rename about page

--- JavaScript/essentials.js
+++ JavaScript/essentials.js
@@ -5,8 +5,8 @@ function load_header() {
 
 function load_navigation() {
 	document.write("<div class=\"topnav\">"
-		+ "<a href=\"index.html\">Home</a>"
-		+ "<a href=\"about.html\">About Me</a>"
+		+ "<a href=\"index.html\">About</a>"
+		+ "<a href=\"projects.html\">Projects</a>"
 		+ "<a href=\"contact.html\">Contact</a>"
 		+ "</div>")
 }
