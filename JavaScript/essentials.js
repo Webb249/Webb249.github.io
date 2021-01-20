65947efb6b5cbf2d1262a5178bbc71fa964191d2
Webb249 <tom.webb249@gmail.com>
2021-01-20 16:04:07 +0000

    Move files into folders

--- JavaScript/essentials.js
+++ JavaScript/essentials.js
@@ -1,7 +1,12 @@
+function load_header() {
+	document.write("<link rel=\"stylesheet\" href=\"navigation.css\">"
+		+ "<link rel=\"stylesheet\" href=\"page.css\">")
+}
+
 function load_navigation() {
 	document.write("<div class=\"topnav\">"
 		+ "<a href=\"index.html\">Home</a>"
 		+ "<a href=\"about.html\">About Me</a>"
 		+ "<a href=\"contact.html\">Contact</a>"
-		+ "</div>;")
+		+ "</div>")
 }
